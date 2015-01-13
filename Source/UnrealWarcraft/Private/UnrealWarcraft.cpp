8065666189aae7b4220843db11f005fd6dc565c0
rayts5 <Jakob_zs@live.dk>
2015-01-12 18:55:24 -0800

    Added my files to the repository, along with a Gitignore.

---
+++ Source/UnrealWarcraft/Private/UnrealWarcraft.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealWarcraft.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealWarcraft, "UnrealWarcraft" );
---
+++ Source/UnrealWarcraft/Public/UnrealWarcraft.h
@@ -0,0 +1,8 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __UnrealWarcraft_H__
+#define __UnrealWarcraft_H__
+
+#include "Engine.h"
+
+#endif
